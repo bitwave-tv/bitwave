bfd4413fc6eba9d41c2c91971d7d9f7c5d830652
dispatch <dispatchcommit@gmail.com>
2019-03-19 13:41:13 -0700

    Change chatroom to bitwave

--- components/TlkChat.vue
+++ components/TlkChat.vue
@@ -5,7 +5,7 @@
   >
     <div
       id="tlkio"
-      data-channel="murderder"
+      :data-channel="channel"
       data-theme="theme--night"
       style="width:100%;height:100%;"
     ></div>
@@ -13,19 +13,27 @@
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
     name: 'TlkChat',
 
     data() {
-      return {}
+      return {
+        channel: 'default',
+      }
     },
 
     methods: {},
 
     computed: {},
+
+    async mounted() {
+      const host = process.env_production ? 'http://localhost:4000' : 'https://api.bitwave.tv';
+      const { data } = await axios.get(`${host}/api/chat/channel`);
+      console.log(data);
+      this.channel = data.chatroom;
+    },
+
   }
 </script>
-
-<style lang='scss'>
-
-</style>
