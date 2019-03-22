7dc546fc45309cba68358a6d54ae98befbdff564
dispatch <dispatchcommit@gmail.com>
2019-03-22 07:55:11 -0700

    Create userlist component

---
+++ components/UserList.vue
@@ -0,0 +1,122 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :mini-variant="miniVariant"
+    :clipped="clipped"
+    stateless
+    fixed
+    flat
+    dark
+    app
+  >
+    <v-layout
+      fill-height
+      column
+    >
+      <v-flex shrink>
+        <v-list class="pb-0">
+          <v-list-tile
+            class="py-1"
+            v-for="(item, i) in items"
+            :key="i"
+            :to="item.to"
+            router
+            exact
+          >
+            <v-list-tile-action>
+              <v-avatar color="#eee" size="40" class="blue--text" >
+                <v-icon  light :size="item.size" >{{ item.icon }}</v-icon>
+              </v-avatar>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+        <!--<hr/>-->
+      </v-flex>
+
+      <!-- User Channels -->
+      <v-flex>
+        <v-list class="pt-0">
+          <v-list-tile
+            class="py-1"
+            v-for="(user, i) in users"
+            :key="i"
+            :to="user.to"
+            router
+            exact
+          >
+            <v-list-tile-avatar>
+              <img :src="user.avatar" :alt="user.name">
+            </v-list-tile-avatar>
+            <v-list-tile-content>{{ user.name }}</v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+
+      <!-- Minify Panel -->
+      <v-flex
+        shrink
+        :align-self-center="!!miniVariant"
+        :align-self-end="!miniVariant"
+        class="px-2"
+      >
+
+        <v-btn
+          style="min-width: 32px;"
+          color="yellow"
+          block
+          outline
+          @click.stop="toggleMini"
+        >
+          <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-navigation-drawer>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: 'UserList',
+
+    data() {
+      return {
+        drawer: true,
+        clipped: false,
+        miniVariant: true,
+
+        items: [
+          {
+            icon: 'whatshot', //blur_on // ondemand_video // live_tv
+            size: 32,
+            title: 'Welcome',
+            to: '/',
+          },
+        ],
+
+        users: [],
+      }
+    },
+
+    methods: {
+      toggleMini() {
+        this.miniVariant = !this.miniVariant;
+      },
+    },
+
+    computed: {},
+
+    async mounted() {
+      const { data } = await axios.get('https://api.bitwave.tv/api/channels/list');
+      this.users = data.users;
+    },
+  }
+</script>
+
+<style lang='scss'>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -65,82 +65,7 @@
     </v-toolbar>
 
     <!-- L-Nav Drawer -->
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      stateless
-      fixed
-      flat
-      dark
-      app
-    >
-      <v-layout
-        fill-height
-        column
-      >
-        <v-flex shrink>
-          <v-list class="pb-0">
-            <v-list-tile
-              class="py-1"
-              v-for="(item, i) in items"
-              :key="i"
-              :to="item.to"
-              router
-              exact
-            >
-              <v-list-tile-action>
-                <v-avatar color="#eee" size="40" class="blue--text" >
-                  <v-icon  light :size="item.size" >{{ item.icon }}</v-icon>
-                </v-avatar>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-          <!--<hr/>-->
-        </v-flex>
-
-        <!-- User Channels -->
-        <v-flex>
-          <v-list class="pt-0">
-            <v-list-tile
-              class="py-1"
-              v-for="(user, i) in users"
-              :key="i"
-              :to="user.to"
-              router
-              exact
-            >
-              <v-list-tile-avatar>
-                <img :src="user.avatar" :alt="user.name">
-              </v-list-tile-avatar>
-              <v-list-tile-content>{{ user.name }}</v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-flex>
-
-        <!-- Minify Panel -->
-        <v-flex
-          shrink
-          :align-self-center="!!miniVariant"
-          :align-self-end="!miniVariant"
-          class="px-2"
-        >
-
-          <v-btn
-            style="min-width: 32px;"
-            color="yellow"
-            block
-            outline
-            @click.stop="miniVariant = !miniVariant"
-          >
-            <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-          </v-btn>
-        </v-flex>
-      </v-layout>
-    </v-navigation-drawer>
+    <user-list />
 
     <!-- Content -->
     <v-content>
@@ -167,7 +92,8 @@
   import Chat from '~/components/Chat'
   import TlkChat from '~/components/TlkChat'
 
-  import axios from 'axios'
+  import UserList from '~/components/UserList'
+
   import { mapState } from 'vuex'
 
   export default {
@@ -176,6 +102,7 @@
       User,
       Chat,
       TlkChat,
+      UserList,
     },
 
     data() {
@@ -188,22 +115,6 @@
 
         miniVariant: true,
         drawer: true,
-
-        items: [
-          {
-            icon: 'whatshot', //blur_on // ondemand_video // live_tv
-            size: 32,
-            title: 'Welcome',
-            to: '/',
-          },
-          /*{
-            icon: 'apps',
-            title: 'Inspire',
-            to: '/inspire',
-          },*/
-        ],
-
-        users: [],
 
         right: true,
         rightDrawer: true,
@@ -220,10 +131,7 @@
       }),
     },
 
-    async mounted() {
-      const { data } = await axios.get('https://api.bitwave.tv/api/channels/list');
-      this.users = data.users;
-    },
+    mounted() {},
   }
 </script>
 
