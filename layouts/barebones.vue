89a589569dfaf31d90eea8461f0fb70904cdb46e
dispatch <dispatchcommit@gmail.com>
2019-04-04 04:15:58 -0700

    Create popout chat component

---
+++ layouts/barebones.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-app :dark="dark">
+
+    <!-- Toolbar -->
+    <v-toolbar
+      :clipped-left="clipped"
+      clipped-right
+      flat
+      dark
+      dense
+      fixed
+      app
+    >
+      <!--<v-toolbar-side-icon @click="drawer = !drawer">
+        <v-icon large>devices</v-icon>
+      </v-toolbar-side-icon>-->
+
+      <!--<v-toolbar-side-icon @click="drawer = !drawer" />-->
+
+      <v-toolbar-title class="ml-0 mr-1">
+        <v-btn
+          to="/"
+          depressed
+          flat
+          exact
+          active-class
+          class="text-none title px-2"
+        >{{ title }}</v-btn>
+      </v-toolbar-title>
+
+      <v-spacer />
+
+      <user/>
+
+      <v-btn
+        v-show="!$vuetify.breakpoint.mdAndUp"
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>menu</v-icon>
+      </v-btn>
+
+    </v-toolbar>
+
+    <!-- Content -->
+    <v-content style="max-height: 100vh">
+      <nuxt />
+    </v-content>
+
+  </v-app>
+</template>
+
+<script>
+  import User from '~/components/User'
+
+  import { mapState } from 'vuex'
+
+  export default {
+    components: {
+      User,
+    },
+
+    data() {
+      return {
+        title: 'BitWave.tv',
+
+        dark: true,
+        clipped: false,
+        fixed: false,
+
+        miniVariant: true,
+        drawer: true,
+
+        right: true,
+        rightDrawer: this.$vuetify.breakpoint.smAndUp,
+        width: this.$vuetify.breakpoint.smAndUp ? 450 : 300,
+      }
+    },
+
+    methods: {
+
+    },
+
+    computed: {
+      ...mapState({
+        currentUser: state => state.user.currentUser,
+      }),
+    },
+  }
+</script>
+
+<style lang='scss'>
+  .v-toolbar__content {
+    padding-left: 0;
+    /*padding-right: 0;*/
+  }
+</style>
