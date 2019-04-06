e4ab2529dcd57ca6c084c30a1c8b916116074765
dispatch <dispatchcommit@gmail.com>
2019-04-06 05:29:12 -0700

    Add nuxtClientInit base code

---
+++ plugins/nuxt-client-init.js
@@ -0,0 +1,11 @@
+/*console.log('nuxtClientInit Plugin');
+
+export default async (ctx) => {
+  console.log('nuxtClientInit Starting.');
+
+  if (!!process.client) {
+    await ctx.store.dispatch('nuxtClientInit', ctx);
+    console.log('nuxtClientInit complete.');
+  }
+
+}*/
