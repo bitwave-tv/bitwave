4e874367a98ccd93e1b88c6fc60aea027f232488
dispatch <dispatchcommit@gmail.com>
2019-05-05 23:01:06 -0700

    initial support for mobile devices on watch page

--- pages/_watch.vue
+++ pages/_watch.vue
@@ -2,7 +2,7 @@
   <v-layout row>
     <!-- Video And Description -->
     <v-layout
-      style="padding-right: 450px;"
+      :style=" `padding-right: ${ $vuetify.breakpoint.mdAndUp ? 450 : 0 }px;` "
       column
     >
       <v-flex
@@ -53,6 +53,11 @@
           </v-responsive>
         </v-card>
       </v-flex>
+
+      <v-flex v-if="!$vuetify.breakpoint.mdAndUp">
+        <chat :dark="true" style="max-height: 60vh;"  />
+      </v-flex>
+
       <v-flex class="px-3">
         <v-layout class="mb-2">
           <v-flex shrink>
@@ -80,7 +85,7 @@
     </v-layout>
 
     <!-- Chat -->
-    <v-flex shrink style="position: fixed; top: 48px; right: 0; height: calc(100vh - 48px); width: 450px;">
+    <v-flex shrink style="position: fixed; top: 48px; right: 0; height: calc(100vh - 48px); width: 450px;" v-if="$vuetify.breakpoint.mdAndUp">
       <chat :dark="true" />
     </v-flex>
 
