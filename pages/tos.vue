f2f7b46b202ad706d43c5bc419f41ccf0d3c144f
DispatchCommit <dispatchcommit@gmail.com>
2019-07-09 23:49:01 -0700

    Add TOS

---
+++ pages/tos.vue
@@ -0,0 +1,237 @@
+<template>
+  <v-container>
+    <div>
+      <h1>[bitwave.tv] Terms of Service</h1>
+      <p>
+        This Livestream Terms of Service Agreement (the "Agreement" or "Terms of Service") is made between Bitwave ("bitwave", "bitwave.tv", "bitwave media", "we", "us", or "our") and you, our customer ("you" or "your").
+        Bitwave provides live streaming services through our owned-and-operated websites, applications, and embeddable video players (collectively, the "Services").
+        This Agreement governs your use of our Services.
+      </p>
+
+      <h3>1. Acceptance</h3>
+      <p>
+        By creating an account, viewing videos, making a purchase, downloading our software, or otherwise visiting or using our Services, you accept this Agreement and consent to contract with us electronically.
+      </p>
+      <p>
+        If you are an entity other than a natural person, the person who registers the account or otherwise uses our Services must have the authority to bind the entity. In this context, "you" means both the entity and each person who is authorized to access the account.
+      </p>
+      <p>
+        We may update this Agreement by posting a revised version on our website. By continuing to use our Services, you accept any revised Agreement.
+      </p>
+
+      <h3>2. Our Products and Services</h3>
+      <p>
+        <b>Service License:</b>
+        Subject to the terms herof, we grant you access to our Services. This includes the right to:
+      </p>
+      <ul>
+        <li>Stream videos that you have the right to view;</li>
+        <li>Upload, store, and/or live stream videos, subject to your plan;</li>
+        <li>Embed our embeddable video player on third-party websites; and</li>
+        <li>Use all related functionality that we may provide.</li>
+      </ul>
+      <p>
+        <b>Donations:</b>
+        We may allow you to send donations to or accept donations from other users.
+        To enable this feature, you may be required to use our integrated payment vendor.
+        Transactions are processed through the payment vendor and are governed by its terms of service and privacy policy; funds are not passed through Livestream.
+      </p>
+
+      <h3>3. Accounts</h3>
+      <p>
+        <b>Registration:</b>
+        You may create an account to use certain features we offer (e.g. live streaming videos).
+        To do so, you must provide an email address. You consent to receive notices from bitwave media at this email address.
+      </p>
+      <p>
+        <b>Age Requirement</b>
+        Age Requirements: You must be at least 18 years old to create an account.
+        If you wish to use the Services for a commercial purpose, you must be at least 18 years old.
+        If you are at least the requisite age, but are still a minor in your jurisdiction, you must have your parent or legal guardian's permission.
+        Please have that person read this Agreement with you and consent to it before proceeding.
+      </p>
+      <p>
+        <b>Account Security</b>
+        Account Security: You are responsible for all activity that occurs under your account, including unauthorized activity.
+        You must safeguard the confidentiality of your account credentials.
+        If you are using a computer that others have access to, you must log out of your account after each session.
+        If you become aware of unauthorized access to your account, you must change your password and notify us immediately.
+      </p>
+
+      <h3>4. Acceptable Use Policy</h3>
+      <p>
+        We may allow you to upload, live stream, submit, or publish (collectively, to "submit") content such as videos, recordings, images, and text (collectively, "content").
+        You must ensure that your content, and your conduct, complies with the Acceptable Use Policy set forth in this Section 4.
+        Livestream may (but is not obligated to) monitor your account, content, and conduct, regardless of your privacy settings. Livestream may take all appropriate actions to enforce its rights including removing specific videos or suspending or removing your account.
+      </p>
+
+      <h3>5. Licenses Granted by You</h3>
+      <p>
+        As between you and Bitwave, you own and will retain ownership of all intellectual property rights in and to the content you submit.
+        In order to allow Livestream to host and stream your content, you grant Livestream the permissions set forth below.
+      </p>
+
+      <h4>5.1 Video Content</h4>
+      <p>
+        By submitting a video, you grant Livestream permission to:
+      </p>
+      <ul>
+        <li>Stream the video to end users;</li>
+        <li>Embed the video on third-party websites;</li>
+        <li>Distribute the video via our APIs;</li>
+        <li>Make the video available for download;</li>
+        <li>Transcode the video (create compressed versions of your video file that are optimized for streaming); and</li>
+        <li>Generate stills (i.e., "thumbnails") from your video to represent it (if you have not selected one).</li>
+      </ul>
+      <p>
+        If you have enabled a video privacy setting or disabled downloading or embedding, we will limit distribution of your video pursuant to your selection.
+        By enabling access to your video to any third party, you grant each such person permission to stream (and/or download or embed, as applicable) your video.
+        For the purposes of this Section 6.1, your video includes its title, description, tags, and other metadata.
+      </p>
+      <p>
+        The license period begins when you submit the video to Livestream and ends when you or Livestream delete it;
+        provided that Livestream may retain archival copies:
+        (a) for a limited period of time in case you wish to restore it;
+        (b) when the video is the subject of a takedown notice or other legal claim; or
+        (c) when Livestream in good faith believes that it is legally obligated to do so.
+      </p>
+
+      <h3>5.2 Profile</h3>
+      <p>
+        You grant bitwave permission to use your name, likeness, biography, trademarks, logos, or other identifiers used by you in your account profile for the purpose of displaying such properties to the public or the audiences you have specified.
+      </p>
+
+      <h3>5.3 Other Content</h3>
+      <p>
+        Content that is not covered by the licenses set forth in Sections 5.1 or 5.2 shall be governed by this Section 5.3 (e.g., text you submit in chat).
+        You grant Livestream a perpetual and irrevocable right and license to copy, transmit, distribute, publicly perform, and display such content through online means in connection with our Services.
+        If you make suggestions to bitwave on improving or products or services, bitwave may use your suggestions without any compensation to you.
+      </p>
+
+      <h3>6. Prohibited Technical Measures</h3>
+      <p>
+        You will not:
+
+        Except as authorized by law or as permitted by us: scrape, reproduce, redistribute, create derivative works from, decompile, reverse engineer, alter, archive, or disassemble any part of our Services; or attempt to circumvent any of our security, rate-limiting, filtering, or digital rights management measures;
+        Submit any malicious program, script, or code;
+        Submit an unreasonable number of requests to our servers; or
+        Take any other actions to manipulate, interfere with, or damage our Services.
+      </p>
+
+      <h3>7. Your Obligations</h3>
+      <h3>7.1 Representations and Warranties</h3>
+      <p>
+        For each piece of content that you submit to or through Livestream, you represent and warrant that:
+      </p>
+      <ul>
+        <li>You have the right to submit the content to Bitwave and grant the licenses herein;</li>
+        <li>Bitwave will not need to obtain licenses from any third party or pay royalties to any third party with respect to the streaming or other permitted distribution of the content;</li>
+        <li>The content does not, and will not, infringe any third party's rights, including intellectual property rights and privacy rights; and</li>
+        <li>The content complies with this Agreement and all applicable laws.</li>
+      </ul>
+
+      <h3>7.2 Indemnification</h3>
+      <p>
+        You will indemnify, defend, and hold harmless Bitwave and its subsidiaries, parents, affiliates, directors, officers, employees, and agents, from and against all third-party actions arising from:
+        (a) the content you submit to or through the Services; and
+        (b) allegations of actions or omissions by you that (regardless if proven) would constitute a breach of this Agreement.
+      </p>
+
+      <h3>8. Termination</h3>
+      <p>
+        If you breach this Agreement, Bitwave may, at its option:
+        (a) terminate this Agreement immediately, with or without advance written notice;
+        (b) suspend, delete, or limit access to your account or any content within it; and
+        (c) to the extent permitted by applicable law, retain any amounts payable to you (which you forfeit).
+        If Bitwave deletes your account for breach, you may not re-register.
+        In the event of any termination or expiration, the following sections will survive:
+        Section 7.2 (Indemnification),
+        Section 9 (Disclaimers),
+        Section 10 (Limitation of Liability),
+        and Section 11 (General Provisions).
+      </p>
+
+      <h3>9. Disclaimers</h3>
+      <p>
+        BITWAVE PROVIDES THE SERVICES ON AN "AS IS" AND "AS AVAILABLE" BASIS. YOU USE THE SERVICES AT YOUR OWN RISK. You must provide your own device and Internet access.
+        EXCEPT AS EXPRESSLY SET FORTH HEREIN, AND TO THE EXTENT PERMITTED BY APPLICABLE LAW, BITWAVE DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+        Among other things, Bitwave makes no representations or warranties:
+      </p>
+      <ul>
+        <li>That our Services, or any part thereof, will be available or permitted in your jurisdiction, uninterrupted or error-free, or accessible from all devices or browsers;</li>
+        <li>Concerning any content submitted by our users;</li>
+        <li>That any geo-filtering or digital rights management solution that we might offer will be effective;</li>
+        <li>That our Services will meet your business or professional needs;</li>
+        <li>That we will continue to support any particular feature or maintain backwards compatibility with any third-party software or device; or</li>
+        <li>Concerning any third-party websites and resources.</li>
+      </ul>
+
+      <h3>10. Limitation of Liability</h3>
+      <p>
+        <b>TO THE EXTENT PERMITTED BY APPLICABLE LAW:</b>
+        BITWAVE SHALL NOT BE LIABLE FOR ANY INDIRECT, INCIDENTAL, SPECIAL, CONSEQUENTIAL, OR EXEMPLARY DAMAGES, INCLUDING DAMAGES FOR LOSS OF BUSINESS, PROFITS, GOODWILL, DATA, OR OTHER INTANGIBLE LOSSES;
+      </p>
+
+      <h3>11. General Provisions</h3>
+      <p>
+        <b>Choice of Law:</b>
+        This Agreement will be governed by the laws of California State and the United States of America (with respect to matters subject to federal jurisdiction such as copyright), without regard to principles of conflicts of law.
+      </p>
+      <p>
+        <b>Choice of Venue; Jury Trial Waiver:</b>
+        Any action relating to this Agreement or your use of our Services must be commenced in the state or federal courts located in California State, United States of America; you consent to the exclusive jurisdiction of those courts.
+        IN ANY SUCH ACTION, BITWAVE AND YOU IRREVOCABLY WAIVE ANY RIGHT TO A TRIAL BY JURY.
+      </p>
+      <p>
+        <b>Reservation of Rights, Severability, Force Majeure:</b>
+        Bitwave reserves all rights not expressly granted herein.
+        Bitwave's rights and remedies are cumulative.
+        No failure or delay by Bitwave in exercising any right will waive any further exercise of that right.
+        If any term of this Agreement is found invalid or unenforceable by a court of competent jurisdiction, that term will be limited or severed.
+        Bitwave will not be liable for any delay or failure caused by a force majeure event.
+      </p>
+      <p>
+        <b>Relationship:</b>
+        You and Bitwave are independent contractors of one another;
+        neither party is an agent, partner, or joint venturer of the other.
+        This Agreement binds the parties and their successors, personal representatives, and assigns.
+        You may not assign this Agreement to any person whose account has been terminated by Bitwave or who is prohibited from registering;
+        any such assignment will be void.
+      </p>
+      <p>
+        <b>Third Parties:</b>
+        We may provide links to and integrations with websites operated by others.
+        The website operator, not Bitwave, is solely responsible for the content thereof, and your use of each such website will be subject to its terms of service.
+        We may disable integrations with any third party at any time, with or without notice.
+        Except as expressly stated herein, nothing in this Agreement confers any right on any third party.
+      </p>
+      <p>
+        <b>Entire Agreement:</b>
+        This Agreement constitutes the entire understanding of the parties and supersedes all prior understandings regarding the subject matter hereof and may not be modified except in accordance with Section 1 or in a document executed by authorized representatives of Bitwave.
+        If you have a signed agreement with Bitwave, any conflicting term of that agreement will prevail over the terms hereof, but only as to the subject matter of that agreement.
+      </p>
+
+      <br>
+
+      <p>
+        <b>Notices:</b>
+        You must send any notices of a legal nature to us by <a href="mailto:dispatchcommit@gmail.com">email</a>.
+      </p>
+
+    </div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'tos',
+
+    data() {
+      return {}
+    },
+
+    methods: {},
+
+    computed: {},
+  }
+</script>
