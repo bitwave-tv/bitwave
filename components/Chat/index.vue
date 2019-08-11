d4ebc54549d133460dfc4c246431caa98adbe819
DispatchCommit <dispatchcommit@gmail.com>
2019-08-11 15:52:55 -0700

    Create ViewerList Component

--- components/Chat/index.vue
+++ components/Chat/index.vue
@@ -14,79 +14,7 @@
         >
           <!-- Viewer List -->
           <v-flex shrink>
-            <!-- Viewer List -->
-            <v-menu
-              v-model="showViewers"
-              :close-on-content-click="false"
-              :nudge-width="250"
-              offset-y
-              bottom
-            >
-              <template #activator="{ on }">
-                <v-chip
-                  v-on="on"
-                  color="yellow"
-                  text-color="black"
-                >
-                  {{ channelViews[page.toLowerCase()] ? channelViews[page.toLowerCase()].total : 0 }}
-                  <v-icon right>account_circle</v-icon>
-                </v-chip>
-              </template>
-
-              <v-card>
-                <v-sheet color="yellow">
-                  <v-layout class="pl-2" align-center>
-                    <v-flex>
-                      <h5 class="black--text body-1">Live Viewers ({{ viewerCount }})</h5>
-                    </v-flex>
-                    <v-flex shrink>
-                      <v-btn
-                        color="black"
-                        flat
-                        icon
-                        pa-0
-                        @click="showViewers = false"
-                      ><v-icon color="black">close</v-icon></v-btn>
-                    </v-flex>
-                  </v-layout>
-                </v-sheet>
-
-                <v-layout style="max-height: 75vh; overflow: auto; overscroll-behavior: contain;">
-                  <v-list
-                    dense
-                    two-line
-                  >
-                    <v-list-tile
-                      avatar
-                      v-for="viewer in viewerList"
-                      :key="viewer.username"
-                    >
-                      <template v-if="viewerList.length > 0">
-                        <v-list-tile-avatar>
-                          <img v-if="!!viewer.avatar" :src="viewer.avatar" :alt="viewer.username">
-                          <v-icon v-else :style="{ background: viewer.color || 'radial-gradient( yellow, #ff9800 )', color: !viewer.color && 'black' }">person</v-icon>
-                        </v-list-tile-avatar>
-                        <v-list-tile-content>
-                          <v-list-tile-title>{{ viewer.username }}</v-list-tile-title>
-                          <v-list-tile-sub-title
-                            v-if="viewer.page && viewer.page.watch && channelViews[ viewer.page.watch.toLowerCase() ]"
-                          >
-                            Chatting in: {{ `${viewer.page.watch} (${ channelViews[ viewer.page.watch.toLowerCase() ].total })` }}
-                          </v-list-tile-sub-title>
-                          <v-list-tile-sub-title
-                            v-else-if="viewer.page && channelViews[ viewer.page.toLowerCase() ]"
-                          >
-                            Just Watching {{`${viewer.page} (${ channelViews[ viewer.page.toLowerCase() ].total })` }}
-                          </v-list-tile-sub-title>
-                          <v-list-tile-sub-title v-else>Getting Soda</v-list-tile-sub-title>
-                        </v-list-tile-content>
-                      </template>
-                    </v-list-tile>
-                  </v-list>
-                </v-layout>
-
-              </v-card>
-            </v-menu>
+            <ViewerList :page="page" />
           </v-flex>
 
           <!-- Chat Label -->
@@ -371,6 +299,7 @@
   import ChatPollVote from '@/components/Chat/ChatPollVote'
 
   import { mapState, mapMutations, mapActions } from 'vuex'
+  import ViewerList from '@/components/Chat/ViewerList'
 
   export default {
     name: 'Chat',
@@ -383,6 +312,7 @@
     },
 
     components: {
+      ViewerList,
       ChatPollVote,
       ChatPoll,
       ChatMessage,
@@ -422,8 +352,6 @@
         selectionTTS: 1,
         rateTTS: 10.00,
         voicesListTTS: [],
-
-        showViewers: false,
 
         showPoll: false,
 
@@ -787,15 +715,9 @@
         _username: 'username',
       }),
 
-      ...mapGetters('chat', {
-        viewerCount: 'viewerCount',
-      }),
-
       ...mapState ('chat', {
         getModeGlobal: 'global',
         getModeTimestamps: 'timestamps',
-        viewers: 'viewerList',
-        channelViews: 'roomViewerList',
       }),
 
       global: {
@@ -826,9 +748,6 @@
         return this.voicesListTTS.map( ( voice, index ) => { return { text: voice.name, value: index } } );
       },
 
-      viewerList () {
-        return this.showViewers ? this.viewers : [];
-      },
     },
 
     created() {
