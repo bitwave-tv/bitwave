62fd9835be1271dee309f9494627280835172716
DispatchCommit <dispatchcommit@gmail.com>
2019-10-16 18:44:46 -0700

    Upgrade to vuetify 2

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -13,3 +13,25 @@ Vue.use(Vuetify, {
     success: colors.green.accent3
   }
 });
+
+export default ctx => {
+  const vuetify = new Vuetify({
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.yellow,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  });
+
+  ctx.app.vuetify = vuetify;
+  ctx.$vuetify = vuetify.framework;
+}
