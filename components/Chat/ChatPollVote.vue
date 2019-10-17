f3c6834f185d62ae404938d93c48c850bbd6e506
DispatchCommit <dispatchcommit@gmail.com>
2019-10-17 11:51:35 -0700

    Update Chat Poll Votes

--- components/Chat/ChatPollVote.vue
+++ components/Chat/ChatPollVote.vue
@@ -11,99 +11,94 @@
             id="poll-vote-body"
             :class="{ 'pa-2': true, 'py-1': showOptions }"
           >
-            <v-layout column>
-              <v-flex class="">
-                <v-layout align-center>
-                  <v-flex shrink>
-                    <v-progress-circular
-                      size="22"
-                      :rotate="-90"
-                      :value="timeLeft"
-                      :indeterminate="timeLeft > 100"
-                      color="yellow"
-                    ></v-progress-circular>
-                  </v-flex>
-                  <v-flex
-                    class="ml-2"
-                    grow
-                  >
-                    <h3>{{ pollData.title }}</h3>
-                  </v-flex>
-                  <v-flex shrink>
-                    <v-btn
-                      small
-                      style="min-width: 0"
-                      color="yellow"
-                      light
-                      outline
-                      @click="showOptions = !showOptions"
-                    >
-                      <v-icon>keyboard_arrow_down</v-icon>
-                    </v-btn>
-                  </v-flex>
-                </v-layout>
-              </v-flex>
 
+            <v-layout align-center>
+              <v-flex shrink>
+                <v-progress-circular
+                  size="22"
+                  :rotate="-90"
+                  :value="timeLeft"
+                  :indeterminate="timeLeft > 100"
+                  color="yellow"
+                ></v-progress-circular>
+              </v-flex>
               <v-flex
-                v-if="!showResults && showOptions"
-                v-for="(val, index) in options"
-                :key="val.label"
-                column
+                class="ml-2"
+                grow
               >
+                <h3>{{ pollData.title }}</h3>
+              </v-flex>
+              <v-flex shrink>
                 <v-btn
                   small
-                  block
-                  light
+                  style="min-width: 0"
                   color="yellow"
-                  :disabled="voted"
-                  @click="vote(index)"
+                  class="black--text"
+                  outlined
+                  @click="showOptions = !showOptions"
                 >
-                  {{ `${index}. ${val.label}` }}
+                  <v-icon>keyboard_arrow_down</v-icon>
                 </v-btn>
               </v-flex>
-
-              <v-flex
-                v-if="( ( showResults || isOwner ) && showOptions )"
-                v-for="(val, index) in options"
-                :key="index"
-              >
-                {{ `${val.label} - ${val.votes} (${Math.round( val.votes / ( pollData.voters || 1 ) * 100 )}%)` }}
-                <v-progress-linear
-                  :value="val.votes / (pollData.voters || 1) * 100"
-                  :buffer-value="(pollData.voters || 1)"
-                  color="yellow"
-                ></v-progress-linear>
-              </v-flex>
-
-              <v-flex v-if="isOwner">
-                <v-layout row>
-                  <v-spacer></v-spacer>
-                  <v-flex shrink>
-                    <v-btn
-                      small
-                      flat
-                      color="yellow"
-                      light
-                      @click="$emit('destroy', pollData.id)"
-                    >
-                      Delete
-                    </v-btn>
-                  </v-flex>
-                  <v-flex shrink>
-                    <v-btn
-                      small
-                      outline
-                      color="yellow"
-                      light
-                      @click="$emit('end', pollData.id)"
-                    >
-                      End Poll
-                    </v-btn>
-                  </v-flex>
-                </v-layout>
-              </v-flex>
-
             </v-layout>
+
+            <v-flex
+              v-if="!showResults && showOptions"
+              v-for="(val, index) in options"
+              :key="val.label"
+              my-2
+            >
+              <v-btn
+                small
+                block
+                class="black--text text-left text-truncate"
+                color="yellow"
+                :disabled="voted"
+                @click="vote(index)"
+              >
+                {{ `${index}. ${val.label}` }}
+              </v-btn>
+            </v-flex>
+
+            <v-flex
+              v-if="( ( showResults || isOwner ) && showOptions )"
+              v-for="(val, index) in options"
+              :key="index"
+              mb-3
+            >
+              {{ `${val.votes} (${Math.round( val.votes / ( pollData.voters || 1 ) * 100 )}%) ${val.label}` }}
+              <v-progress-linear
+                :value="val.votes / (pollData.voters || 1) * 100"
+                :buffer-value="(pollData.voters || 1)"
+                color="yellow"
+              ></v-progress-linear>
+            </v-flex>
+
+            <v-layout justify-end v-if="isOwner">
+              <v-flex shrink>
+                <v-btn
+                  small
+                  text
+                  color="yellow"
+                  class="black--text"
+                  @click="$emit('destroy', pollData.id)"
+                >
+                  Delete
+                </v-btn>
+              </v-flex>
+              <v-flex shrink ml-2>
+                <v-btn
+                  small
+                  outlined
+                  color="yellow"
+                  class="black--text"
+                  @click="$emit('end', pollData.id)"
+                >
+                  End Poll
+                </v-btn>
+              </v-flex>
+            </v-layout>
+
           </v-sheet>
         </v-flex>
       </v-layout>
