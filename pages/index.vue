395393475effd2fa8043a02570deb9700b71f863
DispatchCommit <dispatchcommit@gmail.com>
2019-11-19 11:43:33 -0800

    Update Homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,54 @@
 <template>
   <v-container fluid>
-    <v-layout justify-center>
-      <v-flex
-        xs12
-        sm10
-        md8
+    <v-row class="justify-center">
+      <v-col
+        cols="12"
+        md="10"
+        xl="8"
       >
+        <!-- Banner Stream -->
         <v-card
           v-if="live"
           color="black"
+          class="d-flex align-center flex-wrap elevation-8 mb-2"
         >
-          <v-layout wrap align-center>
-            <v-flex
-              style="min-width: 40%;"
+          <div
+            class="flex-grow-1"
+            style="min-width: 40%;"
+          >
+            <video
+              playsinline
+              id="solo-player"
+              class="video-js vjs-default-skin vjs-fluid"
+              controls
+              autoplay
+              muted
+              preload="auto"
+              data-setup='{ "aspectRatio":"16:9" }'
+              :poster="poster"
             >
-              <video
-                playsinline
-                id="solo-player"
-                class="video-js vjs-default-skin vjs-fluid"
-                controls
-                autoplay
-                muted
-                preload="auto"
-                data-setup='{ "aspectRatio":"16:9" }'
-                :poster="poster"
+              <source
+                :src="live[0].src"
+                :type="live[0].type"
               >
-                <source
-                  :src="live[0].src"
-                  :type="live[0].type"
-                >
-              </video>
-            </v-flex>
+            </video>
+          </div>
 
-            <v-flex
-              shrink
-              :style="{ width: mobile ? '100%' : '450px' }"
-            >
-              <v-layout style="max-height: 500px;">
-                <v-flex>
-                  <chat
-                    :chat-channel="live[0].name"
-                  />
-                </v-flex>
-              </v-layout>
-            </v-flex>
-          </v-layout>
-
+          <div class="d-flex flex-shrink-1" :style="{ width: mobile ? '100%' : '450px', 'max-height': mobile ? '500px' : '655px' }" >
+            <chat :chat-channel="live[0].name" :style="{ height: 'auto' }" />
+          </div>
         </v-card>
 
+        <!-- Live Grid -->
         <stream-grid :streamers="streamers" />
 
-        <v-card class="mb-3">
+        <!-- Homepage Content -->
+        <v-card class="mb-3" color="grey darken-4">
           <v-card-title class="headline pb-0">
-            <h6 class="headline" style="width:100%">Welcome to [bitwave.tv]</h6>
-            <v-layout wrap>
-              <div class="subheading grey--text">
-                An open platform live streaming service for creators to freely express themselves.
-              </div>
-              <v-flex class="ml-2">
+            <div class="d-flex align-center">
+              <div class="mx-3">
                 <v-img
+                  class="d-block"
                   contain
                   max-height="2rem"
                   max-width="2rem"
@@ -66,30 +56,36 @@
                   alt="Cool blobby with sunglasses"
                   @click="updatePlayerSrc"
                 ></v-img>
-              </v-flex>
-            </v-layout>
+              </div>
+              <div class="ml-4">
+                <h6 class="headline" style="width:100%">Welcome to [bitwave.tv]</h6>
+                <div class="subheading grey--text" style="word-break: break-word;">
+                  An open platform live streaming service for creators to freely express themselves.
+                </div>
+              </div>
+            </div>
           </v-card-title>
 
           <v-card-text>
-            <v-flex class="mb-2">
+            <div class="d-flex mb-2 align-center">
               <v-btn
                 small
-                outlined
+                text
                 color="yellow"
                 href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=JAN2HKQ9CTYZY&source=url"
                 target="_blank"
                 class="my-1 mr-1"
               >Donate</v-btn>
-              Want to help support the site?
-            </v-flex>
+              to the bitwave server fund
+            </div>
 
-            <div class="mb-1">A straight forward streaming service built from the ground up on open source software.</div>
+            <div class="mb-1">A straight forward streaming service built entirely on open source software.</div>
             <p>If you're interested in streaming here, just ask for a stream key in any chat, or message us on twitter or discord.</p>
 
-            <v-flex>
+            <div class="d-flex align-center">
               <v-btn
                 small
-                outlined
+                text
                 color="light-blue"
                 href="https://discord.gg/4WwJsKx"
                 target="_blank"
@@ -97,12 +93,12 @@
                 class="my-1 mr-1"
               >Discord</v-btn>
               If you have questions or feedback, join the discord chat.
-            </v-flex>
+            </div>
 
-            <v-flex class="mb-2">
+            <div class="mb-2 d-flex align-center">
               <v-btn
                 small
-                outlined
+                text
                 color="light-blue"
                 href="https://twitter.com/BitWaveTV"
                 target="_blank"
@@ -110,36 +106,36 @@
                 class="my-1 mr-1"
               >Twitter</v-btn>
               Message or tag us on Twitter for direct assistance.
-            </v-flex>
+            </div>
 
-            <v-flex>
+            <div class="d-flex align-center">
               <v-btn
                 small
-                outlined
+                text
                 color="light-blue"
                 href="https://github.com/bitwave-tv/bitwave"
                 target="_blank"
                 class="my-1 mr-1"
               >Github</v-btn>
               For more information on Bitwave's source code.
-            </v-flex>
+            </div>
 
-            <v-flex v-if="false">
+            <div class="d-flex align-center" v-if="false">
               <v-btn
                 small
-                outlined
+                text
                 color="light-blue"
                 href="https://riot.im/app/#/group/+ops:ethanralph.com?hs_url=https://ethanralph.com"
                 target="_blank"
                 title="chat"
               >Riot</v-btn>
               If you have questions, join the riot chat.
-            </v-flex>
+            </div>
 
-            <v-flex>
+            <div class="d-flex align-center">
               <v-btn
                 small
-                outlined
+                text
                 color="light-blue"
                 href="https://github.com/bitwave-tv/bitwave/issues"
                 target="_blank"
@@ -147,31 +143,31 @@
                 class="my-1 mr-1"
               >issue</v-btn>
               Found a bug? Report it on Github.
-            </v-flex>
+            </div>
 
-            <v-flex class="mt-2">
+            <div class="mt-2 d-flex align-center">
               <v-btn
                 small
-                outlined
+                text
                 color="red"
                 to="/report"
                 class="my-1 mr-1"
               >report</v-btn>
               Need to report content?
-            </v-flex>
+            </div>
 
-            <v-flex class="mb-2">
+            <div class="mb-2 d-flex align-center">
               <v-btn
                 small
-                outlined
+                text
                 color="red"
                 to="/tos"
                 class="my-1 mr-1"
               >ToS</v-btn>
               Our terms of service.
-            </v-flex>
+            </div>
 
-            <div class="mb-1">Thank you for helping ALPHA test [bitwave.tv] and I look forward to bringing more exciting features in the future.</div>
+            <div class="mb-1">Thank you for helping <v-chip color="red" outlined small class="mx-1">ALPHA</v-chip> test [bitwave.tv] and I look forward to bringing more exciting features in the future.</div>
             <div class="mb-0">We have not been served any secret court orders and are not under any gag orders.</div>
 
             <div class="text-xs-right">
@@ -179,12 +175,14 @@
             </div>
           </v-card-text>
 
-          <hr class="mb-3">
+          <!--<hr class="mb-3">-->
+
+          <v-divider></v-divider>
 
           <v-card-actions class="pt-0">
-            <v-flex>
+            <v-col>
               <span>Bitwave Media &copy; {{ new Date().getFullYear() }}</span>
-            </v-flex>
+            </v-col>
             <v-spacer/>
             <v-btn
               small
@@ -202,8 +200,8 @@
             >Register</v-btn>
           </v-card-actions>
         </v-card>
-      </v-flex>
-    </v-layout>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
