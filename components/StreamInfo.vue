5277a3b49c8ddb69935715cf90ccb7d27aefbdc2
DispatchCommit <dispatchcommit@gmail.com>
2019-11-24 08:45:24 -0800

    Create streaminfo button

---
+++ components/StreamInfo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+
+      <v-tooltip bottom>
+        <template #activator="{ on }">
+          <v-btn
+            v-on="on"
+            text
+            icon
+            to="/streamkey"
+          >
+            <v-icon>video_call</v-icon>
+          </v-btn>
+        </template>
+        <span>Stream here</span>
+      </v-tooltip>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'StreamInfo',
+
+    data () {
+      return {};
+    },
+
+    methods: {},
+
+    computed: {},
+  };
+</script>
