c202a31484fa0725e73127d947e2d2fb4a495105
DispatchCommit <dispatchcommit@gmail.com>
2019-12-17 02:13:02 -0800

    Create and protect admin route

---
+++ pages/admin.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fluid>
+    <div class="display-1 font-weight-thin yellow--text">Bitwave Admin Portal</div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'admin',
+
+    middleware: [ 'auth', 'admin' ],
+
+    data () {
+      return {};
+    },
+
+    methods: {},
+
+    computed: {},
+  };
+</script>
---
+++ pages/protected.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <v-alert
+      type="error"
+      outlined
+      prominent
+    >
+      <div class="display-1">
+        <strong>Error:</strong>
+        You Attempted to view a restricted resource.
+      </div>
+    </v-alert>
+    <div>
+      <v-btn
+        color="yellow"
+        light
+      >
+        Homepage
+      </v-btn>
+    </div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'protected',
+
+    data() {
+      return {};
+    },
+
+    methods: {},
+
+    computed: {},
+  };
+</script>
+
+<style lang='scss'>
+
+</style>
