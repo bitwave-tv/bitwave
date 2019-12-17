72ef4b441afde81f7f4594d1922fc666a3ebed5a
DispatchCommit <dispatchcommit@gmail.com>
2019-12-17 03:47:14 -0800

    Create basic components

---
+++ components/Admin/ArchiveManager.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card>
+    <v-card-title>Stream Manager</v-card-title>
+    <v-card-text>TODO: Implement.</v-card-text>
+    <v-card-actions>
+      <v-btn
+        color="yellow"
+        light
+        @click="view"
+      >
+        Manage
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'ArchiveManager',
+
+    data () {
+      return {};
+    },
+
+    methods: {
+      view () {
+        this.$toast.error('Archive Manager does not exist.');
+      },},
+
+    computed: {},
+  };
+</script>
--- pages/admin.vue
+++ pages/admin.vue
@@ -1,14 +1,47 @@
 <template>
   <v-container fluid>
     <div class="display-1 font-weight-thin yellow--text">Bitwave Admin Portal</div>
+    <div class="overline">Welcome to hell</div>
+    <v-row>
+
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <stream-manager />
+      </v-col>
+
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <archive-manager />
+      </v-col>
+
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <reports />
+      </v-col>
+
+    </v-row>
+
+    <div>Nothing here works yet. In case you didn't notice...</div>
   </v-container>
 </template>
 
 <script>
+  import Reports from '@/components/Admin/Reports';
+  import StreamManager from '@/components/Admin/StreamManager';
+  import ArchiveManager from '@/components/Admin/ArchiveManager';
+
   export default {
     name: 'admin',
 
     middleware: [ 'auth', 'admin' ],
+
+    components: { ArchiveManager, StreamManager, Reports },
 
     data () {
       return {};
