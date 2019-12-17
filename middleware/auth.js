652cfeb7f9771d79dd4cf4cf373d25d403765ca7
DispatchCommit <dispatchcommit@gmail.com>
2019-12-17 07:56:02 -0800

    Add redirect handling to login middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,12 +3,13 @@
 
 import { VStore } from '@/store';
 
-export default function ( { redirect, store } ) {
+export default function ( { redirect, store, route } ) {
   console.log( 'Middleware[auth]: Checking if logged in...' );
+  console.log(route);
 
   if ( !store.state[VStore.$states.auth] ) {
     console.log( 'Middleware[auth]: User is not logged in - redirecting...' );
-    return redirect( '/login' );
+    return redirect( `/login?redirect=${route.path}` );
   } else {
     console.log( 'Middleware[auth]: User is logged in.' );
   }
