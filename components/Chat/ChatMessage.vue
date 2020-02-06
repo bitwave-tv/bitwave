1039d3f2cb2724c36ed2dac7d1f893ee91c0e1a1
DispatchCommit <dispatchcommit@gmail.com>
2020-02-06 04:32:14 -0800

    Don't cache in the service worker for now

--- components/Chat/ChatMessage.vue
+++ components/Chat/ChatMessage.vue
@@ -9,7 +9,7 @@
       <!-- Todo: add crossorigin -->
       <img
         v-if="!!props.avatar"
-        :src="`${props.avatar}?_bw1`"
+        :src="`${props.avatar}?__bw`"
         :alt="props.username"
         :key="props.username"
         crossorigin
