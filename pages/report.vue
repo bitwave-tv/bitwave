b602b817257edf7e7e6d89219072b564a7f33d65
DispatchCommit <dispatchcommit@gmail.com>
2020-02-16 14:23:39 -0800

    Update report page

--- pages/report.vue
+++ pages/report.vue
@@ -131,7 +131,7 @@
         };
 
         try {
-          const res = await this.$axios.get('https://ipinfo.io/json?token=28731f83a6e092');
+          const res = await this.$axios.get(`https://ipinfo.io/json?token=${process.env.IPINFO_TOKEN}`);
           ipinfo = res.data;
         } catch (e) {
           console.log(e);
@@ -150,28 +150,6 @@
         } catch (e) {
           console.log(e);
           this.success = false;
-        }
-
-        try {
-          const webhook = `https://discordapp.com/api/webhooks/580779019524505602/9HDaiaF6ERhNdAb5pYSd3QAmEHCn7z-i1RIuoKB5A_M5ZePfWqKn3VFyImxDtokd4nOO`
-          const data = {
-            embeds: [
-              {
-                title: 'New Report',
-                author: {
-                  name: this.name,
-                },
-                description: this.report,
-                footer: {
-                  text: '[bitwave.tv]'
-                },
-              }
-            ],
-          };
-          const res = await this.$axios.post(webhook, JSON.stringify(data));
-          if (res.status !== 204) console.log(res);
-        } catch (e) {
-          console.log(e);
         }
 
         this.submitting = false;
