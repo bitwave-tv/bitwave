959607083c26d9e26fb79ab785a21d78c8ce918d
DispatchCommit <dispatchcommit@gmail.com>
2020-02-25 10:37:41 -0800

    Create firebase analytics plugin
    Create Client side firebase-analytics plugin
    Inject analytics into page

---
+++ plugins/firebase-analytics.client.js
@@ -0,0 +1,25 @@
+// Created by xander on 2/25/2020
+import * as firebase from 'firebase/app'
+
+import 'firebase/analytics'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgIwubBz-nTd0mof6l7eklzJk1evuwzhg",
+  authDomain: "bitwave-7f415.firebaseapp.com",
+  databaseURL: "https://bitwave-7f415.firebaseio.com",
+  projectId: "bitwave-7f415",
+  storageBucket: "bitwave-7f415.appspot.com",
+  messagingSenderId: "246532190856",
+  appId: "1:246532190856:web:314a8853ea0f20717ee53d",
+  measurementId: "G-W05DKSF957"
+};
+
+if ( !firebase.apps.length ) {
+  firebase.initializeApp( firebaseConfig );
+}
+
+// Inject Firebase Analytics
+export default async ( { app }, inject ) => {
+  const analytics = firebase.analytics();
+  inject( 'analytics', analytics );
+}
