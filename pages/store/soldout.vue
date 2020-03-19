fea1b039420d4c15ccb52a61ad5c732368343de0
DispatchCommit <dispatchcommit@gmail.com>
2020-03-19 00:13:41 -0700

    Add soldout page

---
+++ pages/store/soldout.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <div>
+      <h1>We're currently sold out of the item your requested.</h1>
+    </div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'soldout',
+
+    data() {
+      return {};
+    },
+
+    methods: {},
+
+    computed: {},
+
+    mounted() {
+
+    },
+  };
+</script>
+
+<style lang='scss'>
+
+</style>
