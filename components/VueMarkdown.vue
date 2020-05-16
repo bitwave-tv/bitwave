a3d9bce7a5a491da6f8a684b9d89582c63645c17
DispatchCommit <dispatchcommit@gmail.com>
2020-05-15 20:34:36 -0700

    v1.17.0 whitelist image embed domains for description.

--- components/VueMarkdown.vue
+++ components/VueMarkdown.vue
@@ -8,10 +8,35 @@
   function renderMarkDown ( data ) {
     const renderer = new marked.Renderer();
     const linkRenderer = renderer.link;
+    const imgRenderer = renderer.image;
+
     renderer.link = ( href, title, text ) => {
       const html = linkRenderer.call( renderer, href, title, text );
       return html.replace(/^<a /, '<a target="_blank" rel="ugc" ');
     };
+
+    const domainRe = /https?:\/\/((?:[\w\d-]+\.)+[\w\d]{2,})/i;
+    const approvedDomains = [
+      'bitwave.tv',
+      'cdn.bitwave.tv',
+      'imgur.com',
+      'i.imgur.com',
+      'i.stack.imgur.com',
+      'chatemotes.neocities.org',
+    ];
+
+    renderer.image = ( href, title, text ) => {
+      const getDomain = ( domain ) => {
+        return domainRe.exec( domain )[1];
+      };
+
+      if ( approvedDomains.includes( getDomain( href ) ) ) {
+        return imgRenderer.call( renderer, href, title, text );
+      }
+
+      return linkRenderer.call(  renderer, href, title, text ) + ` <span class='overline'>(image)</span>`;
+    };
+
     return marked( data, { renderer: renderer, sanitize: true, breaks: true } );
   }
 
