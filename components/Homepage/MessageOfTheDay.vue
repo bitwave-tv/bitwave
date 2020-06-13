1a8770efff1dea9cdf0dd09cc1d859d173575b67
DispatchCommit <dispatchcommit@gmail.com>
2020-06-13 11:54:17 -0700

    update MOTD

--- components/Homepage/MessageOfTheDay.vue
+++ components/Homepage/MessageOfTheDay.vue
@@ -2,8 +2,8 @@
   <v-row class="justify-center">
     <div class="d-flex align-center">
       <div class="title text-center mx-3 d-flex flex-column">
-        <span class="font-weight-light white--text">Freedom of Expression <span class="black--text">#BLM</span></span>
-        <span class="overline grey--text">for livestreamers and people of color</span>
+        <span class="font-weight-light white--text">Freedom of Expression</span>
+        <span class="overline grey--text">for livestreamers and tractors</span>
       </div>
     </div>
   </v-row>
