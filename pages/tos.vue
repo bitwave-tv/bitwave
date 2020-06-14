e9f5ddaa83867d8cd4f76e2c2edf1c17aef27aaa
DispatchCommit <dispatchcommit@gmail.com>
2020-06-14 11:49:21 -0700

    Update ToS, add Privacy Policy

--- pages/tos.vue
+++ pages/tos.vue
@@ -1,6 +1,18 @@
 <template>
   <v-container>
     <div>
+      <h1>Basic Guidelines</h1>
+      <ul>
+        <li>1) You must to be at least 18 or older</li>
+        <li>2) No exploiting minors or CP / CSAM (<strong>including loli</strong>)</li>
+        <li>3) No 24/7 pornography streams</li>
+        <li>4) Do not <strong>promote</strong> or <strong>endorse</strong> "furry" content (criticism / commentary is ok)</li>
+        <li>Guidelines are subject to change</li>
+      </ul>
+      <p>
+        If you have questions or concerns about the content of your streams, please reach out to us: <a href="mailto:support@bitwave.tv">support@bitwave.tv</a>.
+      </p>
+
       <h1>[bitwave.tv] Terms of Service</h1>
       <p>
         This Bitwave Terms of Service Agreement (the "Agreement" or "Terms of Service") is made between Bitwave ("bitwave", "bitwave.tv", "bitwave media", "we", "us", or "our") and you, our customer ("you" or "your").
@@ -219,7 +231,68 @@
 
       <p>
         <b>Notices:</b>
-        You must send any notices of a legal nature to us by <a href="mailto:dispatchcommit@gmail.com">email</a>.
+        You must send any notices of a legal nature to us by <a href="mailto:support@bitwave.tv">email</a>.
+      </p>
+
+      <h1>Returns & Refunds</h1>
+      <p>
+        RETURNS
+
+        ----
+
+        Our policy lasts 30 days. If 30 days have gone by since your purchase, unfortunately we can’t offer you a refund or exchange.
+
+        To be eligible for a return, your item must be unused and in the same condition that you received it. It must also be in the original packaging.
+
+        Several types of goods are exempt from being returned. Perishable goods such as food, flowers, newspapers or magazines cannot be returned. We also do not accept products that are intimate or sanitary goods, hazardous materials, or flammable liquids or gases.
+
+        Additional non-returnable items:
+        * Gift cards
+        * Downloadable software products
+        * Some health and personal care items
+
+        To complete your return, we require a receipt or proof of purchase.
+
+        Please do not send your purchase back to the manufacturer.
+
+        There are certain situations where only partial refunds are granted: (if applicable)
+        * Book with obvious signs of use
+        * CD, DVD, VHS tape, software, video game, cassette tape, or vinyl record that has been opened.
+        * Any item not in its original condition, is damaged or missing parts for reasons not due to our error.
+        * Any item that is returned more than 30 days after delivery
+
+        Refunds (if applicable)
+        Once your return is received and inspected, we will send you an email to notify you that we have received your returned item. We will also notify you of the approval or rejection of your refund.
+        If you are approved, then your refund will be processed, and a credit will automatically be applied to your credit card or original method of payment, within a certain amount of days.
+
+        Late or missing refunds (if applicable)
+        If you haven’t received a refund yet, first check your bank account again.
+        Then contact your credit card company, it may take some time before your refund is officially posted.
+        Next contact your bank. There is often some processing time before a refund is posted.
+        If you’ve done all of this and you still have not received your refund yet, please contact us at <a href="mailto:support@bitwave.tv">support@bitwave.tv</a>
+
+        Sale items (if applicable)
+        Only regular priced items may be refunded, unfortunately sale items cannot be refunded.
+
+        Exchanges (if applicable)
+        We only replace items if they are defective or damaged.  If you need to exchange it for the same item, send us an email at <a href="mailto:support@bitwave.tv">support@bitwave.tv</a> and send your item to: 1 Mustang Dr, San Luis Obispo, CA, 93405, United States.
+
+        Gifts
+        If the item was marked as a gift when purchased and shipped directly to you, you’ll receive a gift credit for the value of your return. Once the returned item is received, a gift certificate will be mailed to you.
+
+        If the item wasn’t marked as a gift when purchased, or the gift giver had the order shipped to themselves to give to you later, we will send a refund to the gift giver and he will find out about your return.
+
+        Shipping
+        To return your product, you should mail your product to: 1 Mustang Dr, San Luis Obispo, CA, 93405, United States.
+
+        You will be responsible for paying for your own shipping costs for returning your item. Shipping costs are non-refundable. If you receive a refund, the cost of return shipping will be deducted from your refund.
+
+        Depending on where you live, the time it may take for your exchanged product to reach you, may vary.
+
+        If you are shipping an item over $75, you should consider using a trackable shipping service or purchasing shipping insurance. We don’t guarantee that we will receive your returned item.
+      </p>
+      <p>
+        Contact support if you have questions or concerns: <a href="mailto:support@bitwave.tv">support@bitwave.tv</a>
       </p>
 
     </div>
