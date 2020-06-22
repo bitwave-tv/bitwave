91a0acda2e297b6c4c44ba454ea88a00d925d376
DispatchCommit <dispatchcommit@gmail.com>
2020-06-22 15:06:55 -0700

    update MOTD. mixer is kill.

--- components/Homepage/MessageOfTheDay.vue
+++ components/Homepage/MessageOfTheDay.vue
@@ -2,8 +2,8 @@
   <v-row class="justify-center">
     <div class="d-flex align-center">
       <div class="title text-center mx-3 d-flex flex-column">
-        <span class="font-weight-light white--text">Freedom of Expression <span class="red--text">#AEG</span></span>
-        <span class="overline grey--text">for livestreamers and tractors</span>
+        <span class="font-weight-light white--text">Freedom of Expression</span>
+        <span class="overline grey--text">for livestreamers and mixer refugees</span>
       </div>
       <img crossorigin="" height="52" data-emote-tooltip=":trout:" title=":trout:" alt=":trout:" src="https://cdn.bitwave.tv/static/emotes/trout-52.gif?4">
     </div>
