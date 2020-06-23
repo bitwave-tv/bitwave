a741a9c8e1aadf5cbaf28ddd760e40f5094d7d13
DispatchCommit <dispatchcommit@gmail.com>
2020-06-23 00:00:27 -0700

    Breakout replay card info

--- components/Replay/ReplayCard/index.vue
+++ components/Replay/ReplayCard/index.vue
@@ -1,82 +1,39 @@
 <template>
-  <div>
-    <v-card
-      class="stream-card"
-      :to="link"
-      no-prefetch
-      color="accentwave"
-    >
-      <!-- Replay Card Thumbnails -->
-      <replay-card-thumbnails
-        :thumbnails="thumbnails || [ thumbnail ] "
-        :duration="duration"
-        :time-ago="timeAgo"
-        :timestamp="timestamp"
-        :blur="blur"
-      />
+  <v-card
+    class="stream-card"
+    :to="link"
+    no-prefetch
+    color="accentwave"
+  >
+    <!-- Replay Card Thumbnails -->
+    <replay-card-thumbnails
+      :thumbnails="thumbnails || [ thumbnail ] "
+      :duration="duration"
+      :time-ago="timeAgo"
+      :timestamp="timestamp"
+      :blur="blur"
+    />
 
-      <!-- Replay Data -->
-      <div
-        class="pa-2"
-      >
-        <!-- Replay Title -->
-        <div class="d-flex align-center">
-          <div
-            class="body-1 font-weight-bold text-truncate text-no-wrap primary---text mb-0"
-            :title="title"
-          >{{ title }}</div>
-
-          <template v-if="views">
-            <v-spacer/>
-            <div
-              class="d-flex align-center caption ml-2"
-              :title="`${views} ${views === 1 ? 'view' : 'views'}`"
-            >
-              <div class="grey--text">{{ views < 1 ? '🌊' : views }}</div>
-              <v-icon color="grey" small class="ml-1">visibility</v-icon>
-            </div>
-          </template>
-        </div>
-
-        <!-- Replay Bottom Text -->
-        <div class="caption d-flex align-center">
-
-          <!-- Streamer Username -->
-          <div class="d-block text-truncate grey--text">{{ username }}</div>
-
-          <!-- NSFW Indicator -->
-          <template v-if="nsfw">
-            <v-divider vertical class="mx-2"/>
-            <div class="d-flex align-center">
-              <div class="red--text font-weight-bold">NSFW</div>
-            </div>
-          </template>
-
-          <!-- Comment Count -->
-          <template>
-            <v-spacer/>
-            <div
-              class="grey--text"
-              :title="`${commentCount} ${commentCount === 1 ? 'comment' : 'comments'}`"
-            >
-              {{ commentCount }}
-              <v-icon small color="grey">message</v-icon>
-            </div>
-          </template>
-
-        </div>
-      </div>
-    </v-card>
-  </div>
+    <!-- Replay Card Info -->
+    <replay-card-info
+      :title="title"
+      :username="username"
+      :nsfw="nsfw"
+      :views="views"
+      :comment-count="commentCount"
+    />
+  </v-card>
 </template>
 
 <script>
   import ReplayCardThumbnails from '@/components/Replay/ReplayCard/ReplayCardThumbnails';
+  import ReplayCardInfo from '@/components/Replay/ReplayCard/ReplayCardInfo';
 
   export default {
     name: 'ReplayCard',
 
     components: {
+      ReplayCardInfo,
       ReplayCardThumbnails,
     },
 
@@ -94,18 +51,6 @@
       timestamp    : { type: Date },
       timeAgo      : { type: String },
       blur         : { type: Boolean, default: true },
-    },
-
-    data () {
-      return {
-
-      }
-    },
-
-    computed: {},
-
-    methods: {
-
     },
   }
 </script>
