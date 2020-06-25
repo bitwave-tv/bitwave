9252dbdba42f2e9e9b33020328b1116ab542c05c
DispatchCommit <dispatchcommit@gmail.com>
2020-06-24 22:38:38 -0700

    Create Dashboard page

---
+++ pages/dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'dashboard',
+
+    middleware: 'auth',
+
+    data() {
+      return {};
+    },
+
+    methods: {},
+
+    computed: {},
+
+    mounted() {
+
+    },
+  };
+</script>
+
+<style lang='scss'>
+
+</style>
