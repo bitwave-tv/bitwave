b13fcba64686a356431a048fc5e96f2d5eac1060
DispatchCommit <dispatchcommit@gmail.com>
2020-07-16 06:21:06 -0700

    Update MOTD
    Drops "freedom of expression" in favor of "open platform"

--- components/Homepage/MessageOfTheDay.vue
+++ components/Homepage/MessageOfTheDay.vue
@@ -2,8 +2,10 @@
   <v-row class="justify-center">
     <div class="d-flex align-center">
       <div class="title text-center mx-3 d-flex flex-column">
-        <span class="font-weight-light white--text">Freedom of Expression</span>
-        <span class="overline grey--text">for livestreamers and mixer refugees</span>
+        <span class="font-weight-light white--text">Open Platform Livescreaming</span>
+        <div>
+          <span class="overline font-weight-black grey--text rgb-text">viewer discretion is advised</span>
+        </div>
       </div>
       <img crossorigin="" height="52" data-emote-tooltip=":trout:" title=":trout:" alt=":trout:" src="https://cdn.bitwave.tv/static/emotes/trout-52.gif?4">
     </div>
