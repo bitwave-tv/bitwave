53478a9660a10301e364809dbe9eaa5f59ad8540
Dispatch <45262335+DispatchCommit@users.noreply.github.com>
2020-07-31 01:32:43 -0700

    Fix alignment

--- components/Chat/AutocompleteChat.vue
+++ components/Chat/AutocompleteChat.vue
@@ -1,6 +1,6 @@
 <template>
   <v-sheet
-    class="chat-autocomplete body-2 pl-1 ml-3 py-1"
+    class="chat-autocomplete body-2 py-1"
     color="grey darken-4"
   >
 
@@ -120,7 +120,7 @@
   .chat-autocomplete {
     position: absolute;
     top: 0;
-    transform: translate( 0%, -100% );
+    transform: translate( 5%, -100% );
     width: 90%;
     max-height: 300px;
     overflow-y: hidden;
