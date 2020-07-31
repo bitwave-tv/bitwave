3225e0f699e8f8dcc46a7d2182ec539cd6666046
Mile <milevuletic@national.shitposting.agency>
2020-07-31 02:11:09 +0200

    Added new stat rate options to menu

--- components/Chat/ChatSettings/ChatSettingsPanel/ChatBetaSettingsPanel.vue
+++ components/Chat/ChatSettings/ChatSettingsPanel/ChatBetaSettingsPanel.vue
@@ -5,15 +5,18 @@
     class="mb-3 mx-3"
   >
     <!-- Beta Settings Toggles -->
-    <div>
+    <div id="settings">
       <v-switch
         v-model="trackMetrics"
+        class="mt-3"
         label="Keep track of chat statistics"
         color="primary"
         hide-details
         dense
         inset
       />
+
+      <v-divider id="divider" />
 
       <v-switch
         v-model="trackMetricsPerUser"
@@ -24,6 +27,51 @@
         dense
         inset
       />
+
+      <v-slider
+        :disabled="!trackMetrics"
+        label="Tick rate"
+        v-model="statTickRate"
+        class="align-center"
+        :max="500"
+        :min="1"
+        :step="1"
+        hide-details
+      >
+        <template #append>
+          <v-text-field
+            v-model="statTickRate"
+            class="mt-0 pt-0"
+            hide-details
+            single-line
+            type="number"
+            style="width: 50px"
+          />
+        </template>
+      </v-slider>
+
+      <v-slider
+        :disabled="!trackMetrics"
+        label="Histogram size"
+        v-model="statHistogramSize"
+        class="align-center"
+        :max="50"
+        :min="10"
+        :step="1"
+        hide-details
+      >
+        <template #append>
+          <v-text-field
+            v-model="statHistogramSize"
+            class="mt-0 pt-0"
+            hide-details
+            single-line
+            type="number"
+            style="width: 50px"
+          />
+        </template>
+      </v-slider>
+
     </div>
   </v-card>
 </template>
@@ -43,6 +91,8 @@
       ...mapMutations (Chat.namespace, {
         setTrackMetrics        : Chat.$mutations.setTrackMetrics,
         setTrackMetricsPerUser : Chat.$mutations.setTrackMetricsPerUser,
+        setStatTickRate        : Chat.$mutations.setStatTickRate,
+        setStatHistogramSize   : Chat.$mutations.setStatHistogramSize,
       }),
 
       updateSettings() {
@@ -55,6 +105,8 @@
       ...mapState (Chat.namespace, {
         getTrackMetrics        : Chat.$states.trackMetrics,
         getTrackMetricsPerUser : Chat.$states.trackMetricsPerUser,
+        getStatTickRate        : Chat.$states.statTickRate,
+        getStatHistogramSize   : Chat.$states.statHistogramSize,
       }),
 
       trackMetrics: {
@@ -72,6 +124,35 @@
         },
         get () { return this.getTrackMetricsPerUser }
       },
+
+      statTickRate: {
+        set ( val ) {
+          this.setStatTickRate( val );
+          this.$analytics.logEvent( 'chat_stat_tick_rate', { value: val } );
+        },
+        get () { return this.getStatTickRate }
+      },
+
+      statHistogramSize: {
+        set ( val ) {
+          this.setStatHistogramSize( val );
+          this.$analytics.logEvent( 'chat_stat_histogram_size', { value: val } );
+        },
+        get () { return this.getStatHistogramSize }
+      },
     },
   };
 </script>
+
+<style lang="scss">
+  #settings > * {
+    height: 45px;
+    padding-top: 10px !important;
+    padding-bottom: 10px !important;
+    align-self: center;
+  }
+  #divider {
+    margin: 11.25px 0 11.25px 0 !important;
+    padding: 0 0 0 0 !important;
+  }
+</style>
