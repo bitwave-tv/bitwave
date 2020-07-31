a7b55547f2df5f5d1a7f66522c266e9ab4ddf8b5
Dispatch <45262335+DispatchCommit@users.noreply.github.com>
2020-07-31 01:38:10 -0700

    Revert MOTD changes

--- components/Homepage/MessageOfTheDay.vue
+++ components/Homepage/MessageOfTheDay.vue
@@ -1,10 +1,10 @@
 <template functional>
   <v-row class="justify-center">
     <div class="d-flex align-center">
-      <div class="title text-center mx-3 my-3 d-flex flex-column">
-        <span class="font-weight-bold white--text display-1">Open Platform Livescreaming</span>
-        <div style="margin-top: -10px;">
-          <span class="overline font-weight-black rgb-text" style="font-size: 15pt">viewer discretion is advised</span>
+      <div class="title text-center mx-3 d-flex flex-column">
+        <span class="font-weight-light white--text display-1">Open Platform Livescreaming</span>
+        <div>
+          <span class="overline font-weight-black rgb-text">viewer discretion is advised</span>
         </div>
       </div>
       <img crossorigin="" height="52" data-emote-tooltip=":trout:" title=":trout:" alt=":trout:" src="https://cdn.bitwave.tv/static/emotes/trout-52.gif?4">
