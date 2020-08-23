fe2c91d3dd363ad66ec8277be1a9fbffbe8b46d3
DispatchCommit <dispatchcommit@gmail.com>
2020-08-23 15:42:07 -0700

    Better sidebar tooltips

--- components/SubLayout/sidebar/StreamerList/LiveStreamers.vue
+++ components/SubLayout/sidebar/StreamerList/LiveStreamers.vue
@@ -9,7 +9,7 @@
     <!-- Skeleton loading until fetch completes -->
     <template
       v-if="liveChannelList === null"
-      v-for="i in 4"
+      v-for="i in 6"
     >
       <v-list-item
         :key="i"
@@ -37,36 +37,64 @@
       name="fade-transition"
       tag="div"
     >
-      <v-list-item
+
+      <div
         v-for="( user ) in liveChannelList"
         :key="user.owner"
-        :to="user.to"
-        class="py-1"
-        router
-        exact
-        @click="onClick"
-        no-prefetch
-        :title="`${user.nsfw ? '(NSFW) ' : ''}${user.name}`"
       >
-        <v-list-item-avatar
-          :color="user.live ? user.nsfw ? '#ff4b66' : '#13a9fe' : '#000'"
-          class="my-1"
+        <v-tooltip
+          :open-delay="15"
+          right
+          color="blue-grey darken-4"
+          transition="fade-transition"
         >
-          <v-avatar
-            :size="user.live ? user.nsfw ? 34 : 34 : 38"
+          <template #activator="{ on: tooltip }">
+            <v-list-item
+              v-on="{ ...tooltip }"
+              :key="user.owner"
+              :to="user.to"
+              class="py-1"
+              router
+              exact
+              @click="onClick"
+              no-prefetch
+            >
+              <v-list-item-avatar
+                :color="user.live ? user.nsfw ? '#ff4b66' : '#13a9fe' : '#000'"
+                class="my-1"
+              >
+                <v-avatar
+                  :size="user.live ? user.nsfw ? 34 : 34 : 38"
+                >
+                  <img
+                    :class="{ offline : !user.live }"
+                    :src="user.avatar"
+                    :alt="user.name"
+                  />
+                </v-avatar>
+              </v-list-item-avatar>
+              <v-list-item-content class="py-0">
+                <v-list-item-title>{{ user.title }}</v-list-item-title>
+                <v-list-item-subtitle>{{ user.name }}</v-list-item-subtitle>
+                <v-list-item-subtitle>{{ user.viewCount }} viewers</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+
+          <div
+            :key="user.owner"
           >
-            <img
-              :class="{ offline : !user.live }"
-              :src="user.avatar"
-              :alt="user.name"
-            />
-          </v-avatar>
-        </v-list-item-avatar>
-        <v-list-item-content class="py-0">
-          <v-list-item-title>{{ user.title }}</v-list-item-title>
-          <v-list-item-subtitle>{{ user.name }}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+            <h3>{{ user.title }}</h3>
+            <div class="d-flex">
+              <div v-if="user.nsfw" class="red--text mr-2">NSFW</div>
+              {{ user.name }}
+            </div>
+            <div>{{ user.viewCount }} viewers </div>
+          </div>
+
+        </v-tooltip>
+      </div>
+
     </transition-group>
 
 
@@ -97,6 +125,9 @@
 </template>
 
 <script>
+  import { mapGetters } from 'vuex';
+  import { VStore } from '@/store';
+
   const userUpdateRate = 30;
 
   export default {
@@ -121,8 +152,15 @@
       const getLiveChannelList = async () => {
         this.updatingList = true;
         try {
-          const { data } = await this.$axios.get( 'https://api.bitwave.tv/api/channels/live', { progress: false }  );
-          if ( data && data.hasOwnProperty( 'users' ) ) return  data.users;
+          const { data } = await this.$axios.get( 'https://api.bitwave.tv/v1/channels/live', { progress: false }  );
+          if ( data && data.hasOwnProperty( 'streamers' ) ) {
+            const streamers = data.streamers.map( stream => {
+              return { ...stream, viewCount: this.getChannelViews( stream.name ) || 0 };
+            });
+            // optionally update sorting
+            streamers.sort( ( a, b ) => b.viewCount - a.viewCount );
+            return streamers;
+          }
         } catch ( error ) {
           console.warn( `Failed to update sidebar.`, error.message );
         }
@@ -142,7 +180,11 @@
       },
     },
 
-    computed: {},
+    computed: {
+      ...mapGetters({
+        getChannelViews: VStore.$getters.getChannelViews,
+      }),
+    },
 
     mounted() {
       this.userListTimer = setInterval(
