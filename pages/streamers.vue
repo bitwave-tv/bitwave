85268c3572355b0e09123540b474d3fb16a29499
DispatchCommit <dispatchcommit@gmail.com>
2020-08-25 07:30:11 -0700

    Don't render a card for literally every streamer account
    This wastes a lot of render time and makes page load and performance slow.

--- pages/streamers.vue
+++ pages/streamers.vue
@@ -1,62 +1,96 @@
 <template>
-  <div>
-    <div class="gradient-background">
-      <v-container>
-        <v-autocomplete
-          v-model="model"
-          :label="`Search ${streamers.length} streamers...`"
-          no-data-text="No streamers found"
-          prepend-inner-icon="search"
-          hide-details
-          single-line
-          solo-inverted
-          :items="streamerNames"
-          :search-input.sync="search"
-          @update:search-input="filterStreamers"
-          clearable
-          return-object
-        >
-          <template v-slot:item="data">
-            <v-list-item-avatar>
-              <img
-                :src="`${data.item.avatar}?_bw`"
-                :alt="data.item.name"
-                crossorigin
-              >
-            </v-list-item-avatar>
-            <v-list-item-title>
-              {{ data.item.text }}
-            </v-list-item-title>
-          </template>
-        </v-autocomplete>
+  <div class="gradient-background fill-height">
+    <v-container>
+      <v-autocomplete
+        v-model="model"
+        :label="`Search ${streamers.length} streamers...`"
+        no-data-text="No streamers found"
+        prepend-inner-icon="search"
+        hide-details
+        single-line
+        solo-inverted
+        :items="streamerNames"
+        :search-input.sync="search"
+        class="mt-4"
+        @update:search-input="filterStreamers"
 
-        <v-row>
-          <v-col
-            cols="12"
-            sm="6"
-            md="4"
-            lg="2"
-            v-for="streamer in filteredStreamers"
-            :key="streamer.name"
-          >
-            <v-lazy
-              min-height="150px"
-              :options="{ threshold: 0.5 }"
+        clearable
+        return-object
+      >
+        <template v-slot:item="data">
+          <v-list-item-avatar>
+            <img
+              :src="`${data.item.avatar}?_bw`"
+              :alt="data.item.name"
+              crossorigin
             >
-              <stream-card
-                :to="streamer.to.toString()"
-                :image="`${streamer.live ? streamer.thumbnail : streamer.avatar}`"
-                :live="streamer.live"
-                :nsfw="streamer.nsfw"
-                :title="streamer.title"
-                :name="streamer.name"
-                :viewers="getChannelViews( streamer.name )"
-              />
-            </v-lazy>
-          </v-col>
-        </v-row>
-      </v-container>
-    </div>
+          </v-list-item-avatar>
+          <v-list-item-title>
+            {{ data.item.text }}
+          </v-list-item-title>
+        </template>
+      </v-autocomplete>
+
+
+      <div class="title font-weight-light mt-4">Search Results: {{ filteredStreamers.length }}</div>
+
+
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+          lg="2"
+          v-for="(streamer, index) in filteredStreamers"
+          :key="streamer.name"
+        >
+          <v-lazy
+            min-height="150px"
+            :options="{ threshold: 0.5 }"
+          >
+            <stream-card
+              :key="streamer.name"
+              :to="streamer.to.toString()"
+              :image="`${streamer.live ? streamer.thumbnail : streamer.avatar}`"
+              :live="streamer.live"
+              :nsfw="streamer.nsfw"
+              :title="streamer.title"
+              :name="streamer.name"
+              :viewers="getChannelViews( streamer.name )"
+            />
+          </v-lazy>
+        </v-col>
+      </v-row>
+
+      <!--<div>Literally every streamer:</div>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+          lg="2"
+          v-for="(streamer, index) in streamers"
+          :key="streamer.name"
+        >
+          <v-lazy
+            min-height="150px"
+            :options="{ threshold: 0.5 }"
+          >
+            <stream-card
+              :key="streamer.name"
+              :to="streamer.to.toString()"
+              :image="`${streamer.live ? streamer.thumbnail : streamer.avatar}`"
+              :live="streamer.live"
+              :nsfw="streamer.nsfw"
+              :title="streamer.title"
+              :name="streamer.name"
+              :viewers="getChannelViews( streamer.name )"
+            />
+          </v-lazy>
+        </v-col>
+      </v-row>-->
+
+    </v-container>
   </div>
 </template>
 
@@ -84,7 +118,7 @@
 
         return {
           streamers: data.users,
-          filteredStreamers: data.users,
+          filteredStreamers: [],
         }
       } catch ( error ) {
         console.log( `ERROR: Failed to hydrate page:\n`, error );
@@ -106,9 +140,10 @@
         if ( this.model && this.search ) {
           this.filteredStreamers = this.streamers.filter( s => {
             return s.name.toLowerCase().indexOf( this.search.toLowerCase() ) > -1;
-          });
+          })
+            .slice( 0, 100 );
         } else {
-          this.filteredStreamers = this.streamers;
+          this.filteredStreamers = [];
         }
       },
     },
