884bbc2eee37f9207161c9d9edfac44f4ed3ffd9
DispatchCommit <dispatchcommit@gmail.com>
2020-08-24 18:01:04 -0700

    dev specific MOTD message

--- components/Homepage/MessageOfTheDay.vue
+++ components/Homepage/MessageOfTheDay.vue
@@ -4,7 +4,7 @@
       <div class="title text-center mx-3 d-flex flex-column">
         <span class="font-weight-light white--text">Open Platform Livescreaming</span>
         <div>
-          <span class="overline font-weight-black rgb-text">viewer discretion is advised</span>
+          <span class="overline font-weight-black rgb-text">{{ bottomText }}</span>
         </div>
       </div>
       <img crossorigin="" height="52" data-emote-tooltip=":trout:" title=":trout:" alt=":trout:" src="https://cdn.bitwave.tv/static/emotes/trout-52.gif?4">
@@ -16,5 +16,12 @@
   export default {
     name: 'MessageOfTheDay',
     serverCacheKey: () => 'MOTD',
+    data () {
+      return {
+        bottomText: process.env.BITWAVE_ENV === 'development'
+          ? 'makeshift freedom of expression'
+          : 'viewer discretion is advised';
+      }
+    }
   };
 </script>
